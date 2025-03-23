147e4d1f3a2bec9815690540240f0544e382fdd9
mukhaledd <elneanaay5555@gmail.com>
2025-03-24 00:18:30 +0200

    Implemented transform.

--- CMP3060 Project - Student Version - Linux/source/common/ecs/entity.cpp
+++ CMP3060 Project - Student Version - Linux/source/common/ecs/entity.cpp
@@ -11,8 +11,13 @@ namespace our {
     // To get the local to world matrix, you need to combine this entities matrix with its parent's matrix and
     // its parent's parent's matrix and so on till you reach the root.
     glm::mat4 Entity::getLocalToWorldMatrix() const {
-        //TODO: (Req 8) Write this function
-        return glm::mat4(1.0f);
+        glm::mat4 worldMatrix = localTransform.toMat4();
+        const Entity* current = parent;
+        while (current) {
+            worldMatrix = current->localTransform.toMat4() * worldMatrix;
+            current = current->parent;
+        }
+        return worldMatrix;
     }
 
     // Deserializes the entity data and components from a json object
